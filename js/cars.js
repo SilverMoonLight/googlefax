eaf28403545e71f3a0adf43894a22e96edc7088f
SilverMoonLight <jam072093@gmail.com>
2017-07-13 21:25:48 -0500

    created car page

---
+++ js/cars.js
@@ -0,0 +1,27 @@
+
+function addCars() {
+const inputs = document.getElementsByTagName("input");
+let values = [];
+
+for(let i = 0; i < inputs.length; i++) {
+    inputs[i].value !== "" ? values.push(inputs[i].value) : "";
+}
+
+let container = document.getElementsByClassName("container")[0];
+let newElementContainer = document.createElement("div");
+newElementContainer.className = "newElementContainer";
+const carName = document.createElement("span");
+carName.innerHTML = values[0];
+const carDesc = document.createElement("p")
+carDesc.innerHTML = values[1];
+
+container.appendChild(newElementContainer);
+let headerBar = document.createElement("p");
+headerBar.className = "headBar";
+newElementContainer.appendChild(headerBar);
+newElementContainer.appendChild(carName);
+newElementContainer.appendChild(carDesc);
+
+
+}
+
